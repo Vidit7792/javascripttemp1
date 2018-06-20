41d09a9142e62ad44b215d51b0887a1e002f4e4b
Vidit7792 <39307865+Vidit7792@users.noreply.github.com>
2018-06-20 22:24:37 +0530

    Add files via upload

---
+++ temp.js
@@ -0,0 +1,8 @@
+var temperatureInCelsius = 40;
+var celsiusToFahrenheit= 40*9/5 +32;
+console.log(celsiusToFahrenheit)
+
+
+var temperatureInFahrenheit=140;
+var FahrenheitToCelsius=((104-32)*5)/9
+console.log(FahrenheitToCelsius)
